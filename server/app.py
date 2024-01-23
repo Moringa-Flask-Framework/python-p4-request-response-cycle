3a3f4ffc9230a0bec63564db1e91d799d5a1caad
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-01-23 08:41:21 +0300

    Creating response with http code

--- server/app.py
+++ server/app.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
+import os
+from flask import Flask, request, current_app, g
 
-from flask import Flask
+
 
 app = Flask(__name__)
+
+@app.before_request
+def app_path():
+    g.path= os.path.abspath(os.getcwd())
+
+@app.route('/')
+def index():
+    host= request.headers.get('Host')
+    appname= current_app.name
+    return f'''<h1>The host is {host}</h1>
+    <h2>The name of this app is {appname}</h2>
+    <h3>The path of this app on the users device is {g.path}</h3>
+    ''', \
+        202
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
